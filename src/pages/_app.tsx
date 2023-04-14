6bdfd7c643e01b3032e42496751e2e4c42075dd7
Daniel A. Caballero <danielalejandrocc33@gmail.com>
2023-04-14 14:32:24 +0200

    Profile page with ssg helper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,22 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { type AppType } from "next/app";
 import { type AppProps } from "next/app";
-
+import { Toaster } from "react-hot-toast";
 import { api } from "~/utils/api";
+
+import Head from "next/head";
 
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }: AppProps) => {
   return (
     <ClerkProvider {...pageProps}>
+      <Head>
+        <title>Emojer</title>
+        <meta name="description" content="☁" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Toaster position="bottom-center" />
       <Component {...pageProps} />
     </ClerkProvider>
   );
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,24 +1,14 @@
-import type { User } from "@clerk/nextjs/dist/api";
 import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
-
 import {
   createTRPCRouter,
   privateProcedure,
   publicProcedure,
 } from "~/server/api/trpc";
-
-const filterUserForClient = (user: User) => {
-  return {
-    id: user.id,
-    username: user.username,
-    profileImageUrl: user.profileImageUrl,
-  };
-};
-
 import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
+import { filterUserForClient } from "~/server/helpers/filterUserForClient";
 
 // Create a new ratelimiter, that allows 3 requests per 1 minute
 const ratelimit = new Ratelimit({
@@ -69,7 +59,7 @@ export const postsRouter = createTRPCRouter({
   create: privateProcedure
     .input(
       z.object({
-        content: z.string().emoji().min(1).max(280),
+        content: z.string().emoji("Only emojis are allowed").min(1).max(280),
       })
     )
     .mutation(async ({ ctx, input }) => {
